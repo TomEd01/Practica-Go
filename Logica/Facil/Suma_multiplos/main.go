d1440056224061a535d9c6e7b0dd8fe33c46702a
TomEd01 <l21530369@cancun.tecnm.mx>
2025-07-26 19:52:58 -0500

    Agregando comentarios

--- Logica/Facil/Suma_multiplos/main.go
+++ Logica/Facil/Suma_multiplos/main.go
@@ -6,10 +6,10 @@ func main(){
 	num := 0
 	fmt.Scan(&num)
 	suma := 0
-	for i:=1; i<=num-1;i++{
-		if i%3==0 || i%5==0{
+	for i:=1; i<=num-1;i++{ // Cambiado de num a num-1 para sumar hasta el número anterior
+		if i%3==0 || i%5==0{ // Verifica si es múltiplo de 3 o 5
 			fmt.Print(i," ")
-			suma+=i
+			suma+=i // Suma el número si es múltiplo de 3 o 5
 		}
 	}
 	fmt.Println("= ",suma)
