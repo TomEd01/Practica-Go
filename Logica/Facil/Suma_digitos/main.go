a0bed6e18037fccffcaef51301935969c26d36f1
TomEd01 <l21530369@cancun.tecnm.mx>
2025-07-26 19:52:07 -0500

    Agregando comentarios

--- Logica/Facil/Suma_digitos/main.go
+++ Logica/Facil/Suma_digitos/main.go
@@ -7,10 +7,10 @@ func main(){
 	fmt.Scan(&num)
 	suma := 0
 	for i:=0; num>0;i++{
-		ultimo_digito := 0
-		ultimo_digito = num%10
-		suma+=ultimo_digito
-		num = num /10
+		ultimo_digito := 0 // Variable para almacenar el último dígito
+		ultimo_digito = num%10 // Obtener el último dígito
+		suma+=ultimo_digito // Sumar el último dígito a la suma total
+		num = num /10 // Eliminar el último dígito del número
 	}
 	fmt.Print(suma)
 }
